1d682d9d6fcf93b4af4588d49535efe44a8d3031
Deekline <nicholas.yemets@gmail.com>
2024-11-05 17:41:50 +0100

    Add eza and fzf zsh aliases

---
+++ roles/zsh/files/zsh/eza.zsh
@@ -0,0 +1,2 @@
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias lt="eza --color=always --tree --level=2  --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
